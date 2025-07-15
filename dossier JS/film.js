c1d5eb99c7dde1dbaa6044ef70be5fff5642ddcb
unknown <Stagiaire@STATION-mzykSEx.exchange.ad.afpanet>
2025-07-15 12:54:10 +0200

    clik

--- dossier JS/film.js
+++ dossier JS/film.js
@@ -1,26 +1,60 @@
-constApiKey = "081a3d88a03ffe4241ed3bf62111904f"
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwODFhM2Q4OGEwM2ZmZTQyNDFlZDNiZjYyMTExOTA0ZiIsIm5iZiI6MTc1MjA4MzM4OS42Nywic3ViIjoiNjg2ZWFiYmRhMzhlNmM5MGU5ZWQ2YThhIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.wsFRIMwEEaqw3dq-Hjx1JfvBXOa8JRmQvSls_vB5gys'
+  }
+};
 
 
-fetch("../dossier JSON/cinema.json")
+const urlParams = new URLSearchParams(window.location.search);
+
+const id = urlParams.get('id');
+console.log(id)
+
+
+fetch( `https://api.themoviedb.org/3/movie/${id}?language=fr-FR`, options)
     .then(response => response.json())
     .then(data => {
-        console.log(data)
+      
+
+
+
+    // faire une fonction on click sur le bouton "search" 
+    // diriger sur recherche.html
+    // stocker ce que l'utilisateur met 
+    // chercher par le biais de l'API si le "string" de l'utilisateur est proche d'un film 
+    
+
+
+    function utilisateurFilm(){
+        
+    }
+
+
+
+        function chercherFilm (){
+
+// let text= input_text.value;
+// addEventListener("input", (event) => { })
+
+// oninput = (event) => { }
+
+
+
+        }
+        
 
         document.getElementById("Watch").innerHTML += `
 
-      <div class="col-13  text-white colonne-couleur card bg-secondary rounded border border-danger style="width:20rem;" " id="Watch">
-                   <img src="https://media.themoviedb.org/t/p/w220_and_h330_face/${data.results.poster_path}"
+      <div class="col-13  text-white colonne-couleur card bg-secondary roundedstyle="width:20rem;" " id="Watch">
+                   <img src="https://media.themoviedb.org/t/p/w220_and_h330_face/${data.poster_path}"
                    
                    
                    <p> Where to stream </p>
                     <p> Amazon </p>
                   <p> Find on Amazon </p>
-                  <p>   Watch trailer </p>
-                   <p> Share title </p>
-                  <p>  Ignore </p>
-                   <p> IMDb </p>
-                   <p> Update history </p>
-                   <p> Suggest an edit </p>
+                
 
 
                 </div>
@@ -28,11 +62,11 @@ fetch("../dossier JSON/cinema.json")
 
         document.getElementById("descriptionDuFilm").innerHTML += `
  <div class="col-7  col colonne-couleur text-white " id="descriptionDuFilm">
-                   <h2 class="TITREFILM text-center ">${data.results.title} </h2>
+                   <h2 class="TITREFILM text-center ">${data.title} </h2>
                     <p class="sypno"> Sypnopsis </p>
                    
 
-                    <p class="resume">${data.results.overview}  </p>
+                    <p class="resume">${data.overview}  </p>
                     <p class="acteurs">  </p>
 
                     <p class="seance"> Seance </p> 
@@ -99,3 +133,6 @@ fetch("../dossier JSON/cinema.json")
 
 
     })
+
+
+
--- dossier JS/script.js
+++ dossier JS/script.js
@@ -1,24 +1,49 @@
 // clé api
 
-constApiKey = "081a3d88a03ffe4241ed3bf62111904f"
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwODFhM2Q4OGEwM2ZmZTQyNDFlZDNiZjYyMTExOTA0ZiIsIm5iZiI6MTc1MjA4MzM4OS42Nywic3ViIjoiNjg2ZWFiYmRhMzhlNmM5MGU5ZWQ2YThhIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.wsFRIMwEEaqw3dq-Hjx1JfvBXOa8JRmQvSls_vB5gys'
+  }
+};
 
 
-// fetch de base
-fetch("../dossier JSON/cinema.json")
+const urlParams = new URLSearchParams(window.location.search);
+
+const id = urlParams.get('id');
+console.log(id)
+
+
+// fetch de b
+
+fetch(`https://api.themoviedb.org/3/movie/now_playing?language=fr-FR&page=1`, options)
     .then(response => response.json())
     .then(data => {
         console.log(data)
 
 
+
+
+ function chercherFilm (){
+          
+        }
+        
+
+
+
+
+
         //    boucle pour generer des films (filmPerso)
-        for (i = 0; i < 8; i++) {
+        for (i = 0; i < 10; i++) {
 
 
             // Le Inner HTML modifié
 
             document.getElementById("filmAleatoire").innerHTML += `
 <div class="card bg-dark rounded text-white  m-2" style="width: 12rem;">
-<a href="film.html? id=${data.results[i].id}">  <img src="https://image.tmdb.org/t/p/w220_and_h330_face/${data.results[i].poster_path}" class="card-img-top" />                              </a>
+<a href="film.html?id=${data.results[i].id}">  <img src="https://image.tmdb.org/t/p/w220_and_h330_face/${data.results[i].poster_path}" class="card-img-top" />                              </a>
 
 <div class="card-body">
 <h6 class="card-title">${data.results[i].title}</h6>
@@ -29,14 +54,14 @@ fetch("../dossier JSON/cinema.json")
  }
 
 //  boucle pour FilmPerso: 
-for (pers=13; pers >9; pers--){ 
+for (pers=13; pers >7; pers--){ 
 
 
 
 
             document.getElementById("filmPerso").innerHTML += `
 <div class="card bg-dark text-white m-1" style="width: 12rem;">
-<a href="film.html? id=">
+<a href="film.html?id=">
 <img src="https://image.tmdb.org/t/p/w220_and_h330_face/${data.results[pers].poster_path}" class="card-img-top" /> </a>
 <div class="card-body">
 <p class="card-text">${data.results [pers].vote_average} </p>
@@ -47,12 +72,12 @@ for (pers=13; pers >9; pers--){
 `
 }
 
-for (fin=19; fin>11; fin--){ 
+for (fin=19; fin>9; fin--){ 
 
 
             document.getElementById("listeSuivi").innerHTML += `
 <div class="card bg-dark text-white m-1  " style="width: 12.3rem;  ">
-<a href="film.html? id=">
+<a href="film.html?id=">
 <img src="https://image.tmdb.org/t/p/w220_and_h330_face/${data.results[fin].poster_path}" class="card-img-top" /> </a>
 
 
